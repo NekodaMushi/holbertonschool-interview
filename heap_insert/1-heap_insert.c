840c8a0bceabf9c0e4d65167a4fff87af1ac5e42
NekodaMushi <study.fabien@gmail.com>
2023-09-01 12:41:16 +0200

    heapify

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -11,44 +11,44 @@ heap_t *heapSort(heap_t *node);
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *new_node, *node = *root;
+heap_t *new_node, *node = *root;
 
-    if (*root == NULL)
-    {
-        *root = binary_tree_node(NULL, value);
-        return (*root);
-    }
+if (*root == NULL)
+{
+*root = binary_tree_node(NULL, value);
+return (*root);
+}
 
 
-    heap_t **queue = malloc(100 * sizeof(heap_t *));
-    int q_start = 0, q_end = 0;
+heap_t **queue = malloc(100 * sizeof(heap_t *));
+int q_start = 0, q_end = 0;
 
-    queue[q_end++] = *root;
+queue[q_end++] = *root;
 
-    while (q_start < q_end)
-    {
-        node = queue[q_start++];
-        if (!node->left)
-        {
-            new_node = binary_tree_node(node, value);
-            node->left = new_node;
-            break;
-        }
-        else if (!node->right)
-        {
-            new_node = binary_tree_node(node, value);
-            node->right = new_node;
-            break;
-        }
-        else
-        {
-            queue[q_end++] = node->left;
-            queue[q_end++] = node->right;
-        }
-    }
-    free(queue);
+while (q_start < q_end)
+{
+node = queue[q_start++];
+if (!node->left)
+{
+new_node = binary_tree_node(node, value);
+node->left = new_node;
+break;
+}
+else if (!node->right)
+{
+new_node = binary_tree_node(node, value);
+node->right = new_node;
+break;
+}
+else
+{
+queue[q_end++] = node->left;
+queue[q_end++] = node->right;
+}
+}
+free(queue);
 
-    return heapSort(new_node);
+return heapSort(new_node);
 }
 
 /**
@@ -58,14 +58,14 @@ heap_t *heap_insert(heap_t **root, int value)
  */
 heap_t *heapSort(heap_t *node)
 {
-    int temp;
-    while (node->parent && node->n > node->parent->n)
-    {
-        temp = node->n;
-        node->n = node->parent->n;
-        node->parent->n = temp;
+int temp;
+while (node->parent && node->n > node->parent->n)
+{
+temp = node->n;
+node->n = node->parent->n;
+node->parent->n = temp;
 
-        node = node->parent;
-    }
-    return node;
+node = node->parent;
+}
+return node;
 }
