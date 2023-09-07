6cd8675893d0d7f7daeacc880e0e721b752bb0ea
NekodaMushi <study.fabien@gmail.com>
2023-09-07 16:54:42 +0200

    Nb minimum d'operation

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+def minOperations(n):
+    nb_of_operations = 0
+
+    if n < 2:
+        return 0
+
+    i = 2
+
+    while n > 1:
+        while n % i == 0:
+            nb_of_operations += i
+            n = n // i
+        i += 1
+
+    return nb_of_operations
