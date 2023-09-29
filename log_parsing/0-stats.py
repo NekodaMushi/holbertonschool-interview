caf53a709ef28595538db6e6b0334801c2d97d71
NekodaMushi <study.fabien@gmail.com>
2023-09-29 20:40:37 +0200

    only one line

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -9,6 +9,9 @@ status_count = {200: 0, 301: 0, 400: 0, 401: 0, 404: 0, 405: 0, 500: 0}
 counter = 0
 file_size = 0
 correct_format = 9
+# line_test = (
+#     '78.99.227.220 - [2017-02-05 23:25:51.534767] "GET /projects/260 HTTP/1.1" 401 724'
+# )
 for line in sys.stdin:
     counter += 1
     part = line.split()
@@ -24,3 +27,8 @@ for line in sys.stdin:
             if count > 0:
                 print("{}: {}".format(code, count))
         counter = 0
+
+    print("File size: {}".format(file_size))
+    for code, count in sorted(status_count.items()):
+        print("{}: {}".format(code, count))
+    counter = 0
