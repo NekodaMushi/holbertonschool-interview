efdf73114a316a1e1e90a4846a9f58db81958a14
NekodaMushi <study.fabien@gmail.com>
2023-09-29 20:48:50 +0200

    add missing 403 code

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -5,7 +5,7 @@ script that reads stdin line by line and computes metrics
 
 import sys
 
-status_count = {200: 0, 301: 0, 400: 0, 401: 0, 404: 0, 405: 0, 500: 0}
+status_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 counter = 0
 file_size = 0
 correct_format = 9
