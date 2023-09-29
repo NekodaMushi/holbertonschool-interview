0aa185b3c0feea511a5ccb08a6fb17dae8015e82
NekodaMushi <study.fabien@gmail.com>
2023-09-29 21:03:34 +0200

    cleaning

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -9,25 +9,14 @@ status_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 counter = 0
 file_size = 0
 correct_format = 9
-# your_list = [
-#     '68.249.9.20 - [2017-02-05 23:31:22.452556] "GET /projects/260 HTTP/1.1" 200 711',
-#     "Hello",
-#     '99.196.100.39 - [2017-02-05 23:31:22.954433] "GET /projects/260 HTTP/1.1" 401 658',
-#     '128.230.61.246 - [2017-02-05 23:31:23.258076] "GET /projects/260 HTTP/1.1" Hello 292',
-#     '116.82.223.35 - [2017-02-05 23:31:24.112360] "GET /projects/260 HTTP/1.1" 301 842',
-#     'Holberton - [2017-02-05 23:31:25.003550] "GET /projects/260 HTTP/1.1" 400 12',
-#     '7.179.133.121 - [2017-02-05 23:31:25.003550] "GET /projects/260 HTTP/1.1" 400 12',
-#     '188.213.11.218-[2017-02-05 23:31:21.690755] "GET /projects/260 HTTP/1.1" 401 1000',
-#     '128.230.61.246 - [2017-02-05 23:31:23.258076] "GET /projects/260 HTTP/1.1" 301 292',
-# ]
+
 
 for line in sys.stdin:
     counter += 1
     part = line.split()
     if len(part) != correct_format:
         continue
-    # if not isinstance(part[0], (int, float)):
-    #     continue
+
     try:
         status_code = part[-2].strip()
         file_size += int(part[-1].strip())
