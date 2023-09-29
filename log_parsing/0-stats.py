fa1b5883e59ad452aeeff175719a884b601b28fa
NekodaMushi <study.fabien@gmail.com>
2023-09-29 20:52:33 +0200

    add missing 403 code

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -9,13 +9,13 @@ status_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 counter = 0
 file_size = 0
 correct_format = 9
-# line_test = (
-#     '78.99.227.220 - [2017-02-05 23:25:51.534767] "GET /projects/260 HTTP/1.1" 401 724'
-# )
+
 for line in sys.stdin:
     counter += 1
     part = line.split()
     if len(part) != correct_format:
+        continue
+    if not part[0].isnumeric():
         continue
     status_code = part[-2].strip()
     file_size += int(part[-1].strip())
