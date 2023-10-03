0f8061e07e017aa4ef0bb3b27504552f72bedf0b
NekodaMushi <study.fabien@gmail.com>
2023-10-03 12:11:26 +0200

    fast will get to slow at one point

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -1,14 +1,24 @@
 #include "lists.h"
 
+/**
+ * check_cycle - Checks if a linked list has a cycle.
+ *
+ * This function checks whether a given linked list has a cycle by using
+ * Floyd's Cycle Detection Algorithm.
+ *
+ * @list: A pointer to the head of the linked list.
+ *
+ * Return: 1 if a cycle is detected, 0 otherwise.
+ */
 int check_cycle(listint_t *list) {
   listint_t *slow = list;
   listint_t *fast = list;
 
-  while (fast){
+  while (fast) {
     fast = fast->next->next;
     slow = slow->next;
 
-    if (fast == slow){
+    if (fast == slow) {
       return (1);
     }
   }
