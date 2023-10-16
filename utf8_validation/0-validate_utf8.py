d52be8c77cf7ca88de7766ad000c5d607bd1ef19
NekodaMushi <study.fabien@gmail.com>
2023-10-16 09:23:04 +0200

    hexa format

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@ import codecs
 
 def validUTF8(data):
     # keep 8 least significant bits of each integer
-    byte = bytes([num & 11111111 for num in data])
+    byte = bytes([num & 0xFF for num in data])
 
     try:
         valid = codecs.decode(byte, "utf-8")
