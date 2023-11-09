dd49c2af815c861a911262112c9d5305e1660a7e
NekodaMushi <study.fabien@gmail.com>
2023-11-09 11:19:48 +0100

    menger moi menger moi menger moi

---
+++ menger/0-menger.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * menger - Draws a 2D Menger Sponge
+ * @level: the level of the Menger Sponge to draw
+ */
+
+void menger(int level) {
+    if (level < 0) {
+        return;
+    }
+
+    int size = pow(3, level);
+    int i, j, block, i_block, j_block;
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            block = 0;
+            i_block = i;
+            j_block = j;
+
+
+            while (i_block > 0 && j_block > 0) {
+                if (i_block % 3 == 1 && j_block % 3 == 1) {
+                    block = 1;
+                    break;
+                }
+                i_block /= 3;
+                j_block /= 3;
+            }
+
+            if (block) {
+                putchar(' ');
+            } else {
+                putchar('#');
+            }
+        }
+        putchar('\n');
+    }
+}
