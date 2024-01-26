cb77a763cd96f0d0717005a1eb0fc1410c909aae
NekodaMushi <study.fabien@gmail.com>
2024-01-26 10:27:34 +0100

    trying to fix checker with help

--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -1,12 +1,16 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
-int _isdigit(char *c);
 
-#endif /*HOLBERTON_H*/
+void *malloc_checked(unsigned int b);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
