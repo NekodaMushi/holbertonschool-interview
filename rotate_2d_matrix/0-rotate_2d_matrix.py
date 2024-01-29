a617b8cc3e1c21cfa0a4b5fc68eff0e43de1c1f1
Fabien Pineau <98282927+NekodaMushi@users.noreply.github.com>
2024-01-29 21:44:42 +0100

    Update 0-rotate_2d_matrix.py

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Rotate 2D Matrix"""
+"""module to rotate"""
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates a 2D matrix 90 degrees clockwise"""
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    """rotate a 2D matrix at 90 degrees clockwise"""
+    for row in range(len(matrix)):
+        for element in range(row, len(matrix)):
+            tmp = matrix[row][element]
+            matrix[row][element] = matrix[element][row]
+            matrix[element][row] = tmp
 
-    for i in range(n):
-        matrix[i].reverse()
-
+    for row in matrix:
+        row.reverse()
