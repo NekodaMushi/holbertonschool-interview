57d25eea079321dbd5d4f54c38a26869bd34020e
NekodaMushi <study.fabien@gmail.com>
2024-01-29 21:48:10 +0100

    useless checker

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 """
-Rotate 2D Matrix
+Useless
 """
 
 
 def rotate_2d_matrix(matrix):
     """
-    Rotates a 2D matrix 90 degrees clockwise in-place and prints the elements on separate lines
+    Rotate a 2D matrix
+    :param matrix: The matrix to rotate in place
+    :return: Nothing
     """
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
     for i in range(n):
-        matrix[i].reverse()
+        matrix[i] = matrix[i][::-1]
