5a63c8c9348f62fe4fd940b3ee8031f6adf02de7
Fabien Pineau <98282927+NekodaMushi@users.noreply.github.com>
2024-02-15 17:57:30 +0100

    This is ridiculous betty

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -10,27 +10,27 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow, *fast;
+	listint_t *slow, *fast;
 
-    slow = head;
-    fast = slow;
+	slow = head;
+	fast = slow;
 
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            slow = head;
-            while (slow && fast)
-            {
-                if (slow == fast)
-                    return (slow);
-                slow = slow->next;
-                fast = fast->next;
-            }
-        }
-    }
-    return (NULL);
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow && fast)
+			{
+				if (slow == fast)
+					return (slow);
+				slow = slow->next;
+				fast = fast->next;
+			}
+		}
+	}
+	return (NULL);
 }
