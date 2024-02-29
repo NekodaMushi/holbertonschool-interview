92a9fa0bf2078e25eeb0aa59bac9a7b4d6b63637
NekodaMushi <study.fabien@gmail.com>
2024-02-29 19:29:30 +0100

    making change

---
+++ making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total):
+    """
+    Minimum number of coins to make up total
+    :param coins: list of coin values
+    :param total: total amount to make up
+    :return: minimum number of coins needed or -1 if not possible
+    """
+    if total <= 0:
+        return 0
+
+    min_coins_list = [float("inf")] * (total + 1)
+    min_coins_list[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                min_coins_list[i] = min(min_coins_list[i], min_coins_list[i - coin] + 1)
+
+    return min_coins_list[total] if min_coins_list[total] != float("inf") else -1
